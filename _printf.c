9b7ef005a0e4ba3c8a61e2608230581c0ed6ced1
ouissalouakrim <ouakrimouissal6@gmail.com>
2023-03-27 16:34:59 +0000

    commit bnr

--- _printf.c
+++ _printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main(void){
+int main(void)
+{
 /**
   * _printf - to print specifiers
   *@format: specifier to be printed
   *Return: format
   */
-
-
 }
 int _printf(const char *format, ...)
 {
